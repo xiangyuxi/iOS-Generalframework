6bb052f3fda7883063bccd99730f177202cc8dfd
pgyx-yxiang <yxiang@YX.local>
2017-04-18 16:43:32 +0800

    添加网络请求库，基于AFN的二次封装

---
+++ iOS-Generalframework/Vender(第三方、自定义与业务无关组件等)/AFNetworking+Gf/GFNetworkingProtocol.h
@@ -0,0 +1,15 @@
+//
+//  GFNetworkingProtocol.h
+//  iOS-Generalframework
+//
+//  Created by yxiang on 2017/4/18.
+//  Copyright © 2017年 xyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GFNetworkingProtocol <NSObject>
+
+
+
+@end
